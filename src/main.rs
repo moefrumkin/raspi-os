240023d8d1d59c9ae5b550952edae6fa5a6b5cb1
M-Kron <78219194+M-Kron@users.noreply.github.com>
2021-07-10 18:44:29 -0400

    Add files via upload
    Parse tree code (in progress). Still needs tests, error handling, and functions of the f(x1, x2, ... xn) form

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,51 @@
-#![no_std]
-#![no_main]
-#![feature(global_asm)]
+use trees::tr;
 
-mod panic;
-mod platform;
+fn main(){
+
+	fn parse(v: Vec<&str>) -> trees::Tree<&str> { //uses a list to allow for multi-character functions. Also, I don't want to deal with
+													//rust's godawful strings more than I need to
+		let operators = vec!["^", "/", "*", "-", "+"]; //in order of operations
+		let mut paren_layer = 0u32;
+		let mut operator_index: Option<usize> = None; //operator index in v
+		let mut operator_priority:i32 = -1;//operator index in operators (-1 if operator index is still none)
+		for i in 0..v.len() {
+			let s = v[i];
+						//paren cases
+			if s == "(" {
+				paren_layer += 1;
+			}
+			if s == ")"{
+				if paren_layer <= 0 {
+					panic!("unbalanced parens") //TODO
+				}
+				paren_layer -= 1;
+			}
+
+			//operator cases
+			if paren_layer == 0{
+				let priotiy = operators.iter().position(|x| x == &s);
+				match priotiy {
+					Some (x) => if x as i32 > operator_priority{
+						operator_priority = x as i32;
+						operator_index = Some(i);
+					},
+					None => () //should just do nothing
+				}
+			}
+		}
+		if paren_layer != 0{
+			//panic("unbalanced parens") TODO
+		}
+		match operator_index {
+			Some (x) => return tr(v[x])/parse(v[0..x].to_vec())/parse(v[x+1..].to_vec()),
+			None => ()
+		}
+		if v.len() == 1 {
+			return tr(v[0]);
+		}
+		if v[0] == "(" && v[v.len() - 1] == ")" {
+			return parse(v[1..v.len()-1].to_vec());
+		}
+		return tr(""); //TODO error message
+	}
+}
