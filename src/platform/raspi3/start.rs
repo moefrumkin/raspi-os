147c57af7e3eb9b96b5c7e44a48ce8464d9b9a6d
Moe Frumkin <moegf1@gmail.com>
2021-07-24 16:04:29 -0400

    gpu updates

--- src/platform/raspi3/start.rs
+++ src/platform/raspi3/start.rs
@@ -5,9 +5,9 @@ use super::gpu;
 
 #[no_mangle]
 pub extern "C" fn start() {
-    /*let fb = gpu::init();
+    let fb = gpu::init();
     
     if let Ok(fbi) = fb {
         fbi.draw();
-    }*/
+    }
 }
