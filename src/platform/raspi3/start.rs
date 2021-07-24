68bac2e2d78ea2974a445e1b3e53bae331e8cf2f
Moe Frumkin <moegf1@gmail.com>
2021-07-24 17:23:13 -0400

    prepare for uart"

--- src/platform/raspi3/start.rs
+++ src/platform/raspi3/start.rs
@@ -2,12 +2,24 @@
 global_asm!(include_str!("start.s"));
 
 use super::gpu;
+use super::gpio;
+use super::timer;
 
 #[no_mangle]
 pub extern "C" fn start() {
-    let fb = gpu::init();
+    let pin = gpio::Pin::new(23).unwrap();
+    pin.set_mode(gpio::Mode::OUT);
+
+    for i in 0..=10 {
+        pin.set_out(gpio::OutputLevel::High);
+        timer::delay(5000);
+        pin.set_out(gpio::OutputLevel::Low);
+        timer::delay(5000);
+    }
+
+    /*let fb = gpu::init();
     
     if let Ok(fbi) = fb {
         fbi.draw();
-    }
+    }*/
 }
