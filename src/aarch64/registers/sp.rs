fa33fa3244caafe1d71a18863d15b0e1b4b15060
Moe Frumkin <moegf1@gmail.com>
2021-08-12 10:48:02 -0400

    make clippy happy

--- src/aarch64/registers/sp.rs
+++ src/aarch64/registers/sp.rs
@@ -1,15 +1,13 @@
-mod sp {
-    pub struct Register {}
+pub struct Register {}
 
-    #[allow(dead_code)]
-    impl Register {
-        pub fn write(&self, value: usize) {
-            unsafe {
-                asm!("mov sp, {}", in(reg) value);
-            }
+#[allow(dead_code)]
+impl Register {
+    pub fn write(&self, value: usize) {
+        unsafe {
+            asm!("mov sp, {}", in(reg) value);
         }
     }
 }
 
 #[allow(dead_code)]
-pub static SP: sp::Register = sp::Register {};
+pub static SP: Register = Register {};
